a8cf26f9b4cd2a1a7cd395f7a46af3ec0a0dd005
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-27 02:25:03 -0500

    Created logic so function determines if the final state of array is too chaotic

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
 console.log('New Year Chaos!');
+
+const minimumBribes = (q) => {
+
+	for (let len = q.length, i = len - 1; i >= 0; i --) {
+
+		if (q[i] - (i + 1) >= 3) {
+			return "Too chaotic";
+		};
+
+	};
+};
+
+console.log(minimumBribes([2, 5, 1, 3, 4]));
