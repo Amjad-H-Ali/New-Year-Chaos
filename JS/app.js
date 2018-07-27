ceecfa5542e8b6df0c222b0c10546cd4d7e6c3ef
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-27 02:58:14 -0500

    Function returns minimum number of bribes!

--- JS/app.js
+++ JS/app.js
@@ -17,8 +17,15 @@ const minimumBribes = (q) => {
 				counter ++;
 			}
 		}
-
 	};
+
+	return counter;
 };
 
-console.log(minimumBribes([2, 1, 5, 3, 4]));
+console.log(minimumBribes([2, 5, 1, 3, 4]));
+
+console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]));
+
+console.log(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]));
+
+console.log(minimumBribes([1, 2, 5, 3, 4, 7, 8, 6]));
